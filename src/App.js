7bc20428d2532e7b0947b19bd0185bc8943eec38
Paulin Trognon <paulin.trognon@gmail.com>
2017-09-11 23:54:03 +0200

    add text field

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout.js';
+import Home from './components/Home/Home';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Layout>
-          hello world
+          <Route path="/" exact={true} component={Home}></Route>
         </Layout>
       </Router>
     );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import './home.css';
+
+export default class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    let newValue = value;
+
+    const i = value.indexOf(',');
+    if(i !== -1) {
+      console.log(value.slice(0, i))
+      newValue = '';
+    }
+
+    this.setState({ text: newValue });
+  }
+
+  handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      console.log(event.target.value);
+      this.setState({ text: '' });
+    }
+  }
+
+  render() {
+    return (
+      <div className="create-draw">
+        <p className="explanations">
+          Entre différents éléments séparés d'une virgule,
+          et clique sur "go" pour en sélectionner un au hasard !
+        </p>
+        <p className="input-container">
+          <input type="text" onChange={this.handleChange.bind(this)} value={this.state.text} onKeyPress={this.handleKeyPress.bind(this)}/>
+        </p>
+      </div>
+    );
+  }
+}
