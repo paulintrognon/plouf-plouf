32408b09d03fb64d3ffa529f7bf6033d4100d41b
Paulin Trognon <paulin.trognon@gmail.com>
2017-09-12 13:07:39 +0200

    move addValueForm to seperate view

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,64 +1,17 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { addValue } from '../../actions/valuesActions';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import './home.css';
+import AddValueForm from '../Values/AddValueForm';
 
-function mapStoreToProps(store) {
-  return {
-    values: store.values,
-  };
-}
-
-class Home extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      text: '',
-    };
-  }
-
-  handleChange(event) {
-    const value = event.target.value;
-    let newValue = value;
-
-    const i = value.indexOf(',');
-    if(i !== -1) {
-      this.props.dispatch(addValue(value.slice(0, i)));
-      newValue = '';
-    }
-
-    this.setState({ text: newValue });
-  }
-
-  handleKeyPress(event) {
-    if (event.key === 'Enter') {
-      this.props.dispatch(addValue(event.target.value));
-      this.setState({ text: '' });
-    }
-  }
-
+class Home extends Component {
   render() {
-    const values = this.props.values;
     return (
-      <div className="create-draw">
-        <p className="explanations">
-          Entre différents éléments séparés d'une virgule,
-          et clique sur "go" pour en sélectionner un au hasard !
-        </p>
-        <p className="input-container">
-          <input type="text"
-            placeholder="Ex: Pomme, Poire"
-            onChange={this.handleChange.bind(this)}
-            onKeyPress={this.handleKeyPress.bind(this)}
-            value={this.state.text}
-            />
-        </p>
-        <ul>
-          {values.map((value, index) => <li key={index}>{value}</li>)}
-        </ul>
+      <div>
+        <AddValueForm></AddValueForm>
       </div>
     );
   }
 }
-export default connect(mapStoreToProps)(Home);
+
+export default Home;
---
+++ src/components/Values/AddValueForm.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addValue } from '../../actions/valuesActions';
+
+import './addValueForm.css';
+
+function mapStoreToProps(store) {
+  return {
+    values: store.values,
+  };
+}
+
+class AddValueForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    let newValue = value;
+
+    const i = value.indexOf(',');
+    if(i !== -1) {
+      this.props.dispatch(addValue(value.slice(0, i)));
+      newValue = '';
+    }
+
+    this.setState({ text: newValue });
+  }
+
+  handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      this.props.dispatch(addValue(event.target.value));
+      this.setState({ text: '' });
+    }
+  }
+
+  render() {
+    const values = this.props.values;
+    return (
+      <div className="create-draw">
+        <p className="explanations">
+          Entre différents éléments séparés d'une virgule,
+          et clique sur "go" pour en sélectionner un au hasard !
+        </p>
+        <p className="input-container">
+          <input type="text"
+            placeholder="Ex: Pomme, Poire"
+            onChange={this.handleChange.bind(this)}
+            onKeyPress={this.handleKeyPress.bind(this)}
+            value={this.state.text}
+            />
+        </p>
+      </div>
+    );
+  }
+}
+export default connect(mapStoreToProps)(AddValueForm);
