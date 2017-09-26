354a3253b4ce0beb0f8f2291e0f3602d86f75056
Paulin Trognon <paulin.trognon@gmail.com>
2017-09-26 19:13:55 +0200

    add bug link

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -6,12 +6,17 @@ export default function () {
   return (
     <footer className="main-footer">
       <a href="https://paulintrognon.fr" target="_blank" rel="noopener noreferrer">
-        Paulin Trognon
+        Paulin Trognon 2017
       </a>
       &nbsp;-&nbsp;
       <a href="https://github.com/paulintrognon/plouf-plouf" target="_blank" rel="noopener noreferrer">
         <i className="fa fa-github" aria-hidden="true"></i>
         &nbsp;Code source
+      </a>
+      &nbsp;-&nbsp;
+      <a href="https://github.com/paulintrognon/plouf-plouf/issues" target="_blank" rel="noopener noreferrer">
+        <i className="fa fa-bug" aria-hidden="true"></i>
+        &nbsp;Bugs
       </a>
     </footer>
   )
