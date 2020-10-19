615ef5a329042ce4ce507b2d5c3410f591b50efa
Paulin Trognon <paulin.trognon@gmail.com>
2020-10-19 19:21:36 +0200

    fix(reset) Remove dangerous 'new' bouton at result screen

--- src/components/DrawResult/ActionButtons/ActionButtons.connect.tsx
+++ src/components/DrawResult/ActionButtons/ActionButtons.connect.tsx
@@ -1,11 +1,10 @@
 import { connect } from 'react-redux'
 import ActionButtons from './ActionButtons'
 import { drawValue } from '../../../redux/features/draw/actions'
-import { reset, backToValuesForm } from '../../../redux/common/actions'
+import { backToValuesForm } from '../../../redux/common/actions'
 
 const dispatchProps = {
   handleOtherResult: drawValue,
-  handleReset: reset,
   handleBack: backToValuesForm,
 }
 
