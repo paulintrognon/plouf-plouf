6e4d751f39f057f6f29651e301e4ae324507a9dc
Paulin Trognon <contact@paulintrognon.fr>
2020-12-23 09:43:35 +0100

    Remove Plausible analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Head from 'next/head'
 import Router from 'next/router'
 import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
@@ -14,14 +13,6 @@ Router.events.on('routeChangeComplete', () => {
 // This default export is required in a new `pages/_app.js` file.
 const App: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => (
   <Provider store={store}>
-    <Head>
-      <script
-        async
-        defer
-        data-domain="plouf-plouf.fr"
-        src="https://analytiques.paulintrognon.fr/js/plausible.js"
-      ></script>
-    </Head>
     <Component {...pageProps} />
   </Provider>
 )
