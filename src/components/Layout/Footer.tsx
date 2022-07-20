cec91df23ffd0ef7b0ebe5307645c6f817698402
Paulin Trognon <paulin.trognon@enexten.com>
2022-07-20 10:47:37 +0200

    Add Canefora banner

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -5,46 +5,53 @@ import styles from './Footer.module.css'
 
 const Footer = () => (
   <footer className={styles.container}>
-    <nav className={styles.nav}>
-      <Link href="/a-propos">
-        <a>À propos</a>
-      </Link>
-    </nav>
-    <div className={styles.bottomLinks}>
-      <a
-        href="https://fr.wikipedia.org/wiki/Copyleft"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="Copyleft"
-      >
-        (ɔ)
-      </a>{' '}
-      <a
-        href="https://paulintrognon.fr"
-        target="_blank"
-        rel="noopener noreferrer"
-        title="Site réalisé par Paulin Trognon"
-      >
-        Paulin Trognon {new Date().getFullYear()}
-      </a>{' '}
-      -{' '}
-      <a
-        href="https://github.com/paulintrognon/plouf-plouf"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fa fa-github" aria-hidden="true"></i>
-        &nbsp;Code source
-      </a>{' '}
-      -{' '}
-      <a
-        href="https://github.com/paulintrognon/plouf-plouf/issues"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i className="fa fa-bug" aria-hidden="true"></i>
-        &nbsp;Bugs &amp; suggestions
+    <div className={styles.adBanner}>
+      <a target="_blank" rel="noopener noreferrer" href="https://www.plouf-plouf.canefora.fr/">
+        ✨ Personnaliser Plouf Plouf pour votre entreprise ✨
       </a>
+    </div>
+    <div className={styles.blueBackground}>
+      <nav className={styles.nav}>
+        <Link href="/a-propos">
+          <a>À propos</a>
+        </Link>{' '}
+      </nav>
+      <div className={styles.bottomLinks}>
+        <a
+          href="https://fr.wikipedia.org/wiki/Copyleft"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Copyleft"
+        >
+          (ɔ)
+        </a>{' '}
+        <a
+          href="https://paulintrognon.fr"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Site réalisé par Paulin Trognon"
+        >
+          Paulin Trognon {new Date().getFullYear()}
+        </a>{' '}
+        -{' '}
+        <a
+          href="https://github.com/paulintrognon/plouf-plouf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa fa-github" aria-hidden="true"></i>
+          &nbsp;Code source
+        </a>{' '}
+        -{' '}
+        <a
+          href="https://github.com/paulintrognon/plouf-plouf/issues"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa fa-bug" aria-hidden="true"></i>
+          &nbsp;Bugs &amp; suggestions
+        </a>
+      </div>
     </div>
   </footer>
 )
