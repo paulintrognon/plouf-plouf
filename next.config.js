84aa7c295f13b7a0032c0f63bef99be49a6da72f
Paulin Trognon <paulin.trognon@enexten.com>
2022-07-20 10:28:48 +0200

    use swcMinify

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 const withOffline = require('next-offline')
 
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
+}
 
 module.exports = withOffline(nextConfig)
